27caf9b70fd1dd5e233087a063951d13b62349d3
kaviramsv <kavitasvenkitachalam@gmail.com>
2022-01-07 14:28:08 +0000

    Add files for  session and user controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20220106232118) do
+ActiveRecord::Schema.define(version: 20220107013151) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,7 +60,7 @@ ActiveRecord::Schema.define(version: 20220106232118) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "email"
-    t.string   "password"
+    t.string   "password_digest"
     t.string   "pasword_confirmation"
     t.datetime "created_at",           null: false
     t.datetime "updated_at",           null: false
